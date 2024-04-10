1a3986b11a1889dcf4f00e31e162802a2178f3ad
Cecilia Ljungquist <c.ljungquist@hotmail.com>
2024-04-10 20:44:49 +0200

    Added fonts and styling for header

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,52 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './routes/Home/index.jsx';
+import Shop from './routes/Shop/index.jsx';
+import About from './routes/About/index.jsx';
+import Cart from './routes/Cart/index.jsx';
+import Contact from './routes/Contact/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // loader: rootLoader,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+        // loader: teamLoader,
+      },
+      {
+        path: "about",
+        element: <About />,
+        // loader: teamLoader,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+        // loader: teamLoader,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+        // loader: teamLoader,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
