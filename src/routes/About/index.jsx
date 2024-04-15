8c8933666cbd5af3189f370ad33642f70ea64355
Cecilia Ljungquist <c.ljungquist@hotmail.com>
2024-04-15 08:31:15 +0200

    Added contact and about page

--- src/routes/About/index.jsx
+++ src/routes/About/index.jsx
@@ -1,6 +1,21 @@
+import './index.css';
+
 function About() {
     return (
-        <h1>About</h1>
+        <div className="about">
+            <h1>About</h1>
+            <section className="about__section">
+                <p>
+                    Lens Legacy is an online webshop specializing in curated posters that aim to elevate living spaces with elegance.
+                </p>
+                <p>
+                    Offering a carefully selected collection, Lens Legacy provides customers with a range of high-quality posters designed to add sophistication and style to any environment.
+                </p>
+                <p>
+                    With a focus on timeless and aesthetically pleasing designs, Lens Legacy caters to individuals looking to enhance their home or office décor with artful pieces that inspire and elevate the atmosphere.
+                </p>
+            </section>
+        </div>
     );
 }
 
--- src/routes/Contact/index.jsx
+++ src/routes/Contact/index.jsx
@@ -1,6 +1,28 @@
+import './index.css';
+
 function Contact() {
     return (
-        <h1>Contact</h1>
+        <div className="contact">
+            <h1>Contact</h1>
+            <section className="contact__section">
+                <p>
+                    Get in touch with Lens Legacy - Elevate Your Space with Elegance. Whether you have questions about our products, need assistance with an order, or simply want to say hello, we're here to help. We value your feedback and are committed to providing exceptional customer service to ensure your experience with Lens Legacy is nothing short of delightful.
+                </p>
+                <p>
+                    To reach out to our friendly team, we invite you to engage in the timeless tradition of letter writing. Address your inquiries, musings, or greetings to:
+                </p>
+                <address>
+                    <p>123 Picture Perfect Lane</p>
+                    <p>Artville, Posterland</p>
+                    <p>ZIP: ART123</p>
+                </address>
+                <p>
+                    Warm regards,
+                    The Lens Legacy Team
+                </p>
+
+            </section>
+        </div >
     );
 }
 
